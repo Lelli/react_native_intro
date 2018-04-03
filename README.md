f2ed90ca8956c2bb38dbd45b54b47ef7c9a15f5a
Simon Alm Liljevall <simon.alm.liljevall@nordnet.se>
2018-04-03 14:41:30 +0200

    Replace lottie animations with Animated

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # React Native Intro
+
 To run the app follow these steps:
-1. Run `yarn`
-2. Run `react-native run-ios` to run the iOS app or `react-native run-android` to run the android app
+
+1.  Run `yarn`
+2.  Run `react-native run-ios` to run the iOS app or `react-native run-android` to run the android app
 
 ## Voting service
+
 For use during the demo, the voting service has been deployed to `https://nameless-depths-10115.herokuapp.com`.
 Available endpoints are:
-1. POST `/vote/{id}` - Vote on specific destination
-2. GET `/results` - Get an array with all results
-3. POST `/reset` - Resets the results
+
+1.  POST `/vote/{id}` - Vote on specific destination
+2.  GET `/results` - Get an array with all results
+3.  POST `/reset` - Resets the results
+
+# Contributions
+
+Icon by `https://www.flaticon.com/authors/good-ware`
--- src/results.js
+++ src/results.js
@@ -1,92 +1,105 @@
-import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Animation from 'lottie-react-native'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Animated, Easing, Image } from "react-native";
+import PropTypes from "prop-types";
 
-import places from './places'
+import places from "./places";
+// import worldImage from "./assets/world.png";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40
   },
   title: {
     fontSize: 20,
-    textAlign: 'center',
-    marginBottom: 20,
+    textAlign: "center",
+    marginBottom: 20
   },
   results: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 20,
-    marginBottom: 20,
+    marginBottom: 20
   },
   animationContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   animation: {
     width: 200,
-    height: 200,
-  },
-})
+    height: 200
+  }
+});
 
 class Results extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isLoading: true,
-      results: [],
-    }
+      results: []
+    };
+    this.spinValue = new Animated.Value(0);
   }
 
   fetchResults = () => {
-    return fetch('https://nameless-depths-10115.herokuapp.com/results')
+    return fetch("https://nameless-depths-10115.herokuapp.com/results")
       .then(response => response.json())
       .then(responseJson => {
-        responseJson = responseJson.sort((a, b) => b.votes - a.votes)
+        responseJson = responseJson.sort((a, b) => b.votes - a.votes);
         // Could hide loader right here, but might be more fun to show it for 6s?
-        setTimeout(() => this.setState({ isLoading: false }), 5000)
-        return this.setState({ results: responseJson })
+        setTimeout(() => this.setState({ isLoading: false }), 5000);
+        return this.setState({ results: responseJson });
       })
       .catch(error => {
-        console.error(error)
-      })
+        console.error(error);
+      });
+  };
+
+  spin() {
+    this.spinValue.setValue(0);
+    Animated.timing(this.spinValue, {
+      toValue: 1,
+      duration: 4000,
+      easing: Easing.linear
+    }).start(() => this.spin());
   }
 
   componentDidMount() {
-    this.animation.play()
-    this.fetchResults()
+    this.spin();
+    this.fetchResults();
   }
 
   render() {
-    const { selectedPlaces } = this.props
-    const { results } = this.state
+    const { selectedPlaces } = this.props;
+    const { results } = this.state;
     if (this.state.isLoading) {
+      const spin = this.spinValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["0deg", "-360deg"]
+      });
       return (
         <View style={styles.animationContainer}>
-          <View style={styles.animation}>
-            <Animation
-              ref={ref => (this.animation = ref)}
-              style={{
-                width: 200,
-                height: 200,
-              }}
-              source={require('./assets/plane.json')}
-              loop={true}
-            />
-          </View>
+          <Animated.Image
+            style={{
+              width: 64,
+              height: 64,
+              transform: [{ rotate: spin }]
+            }}
+            source={require("./assets/world.png")}
+          />
         </View>
-      )
+      );
     }
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Thanks for voting!</Text>
         <Text>You voted on the following destinations:</Text>
-        {selectedPlaces.map(id => <Text key={places[id].id}>{places[id].title}</Text>)}
+        {selectedPlaces.map(id => (
+          <Text key={places[id].id}>{places[id].title}</Text>
+        ))}
         <Text style={styles.results}>Current results</Text>
         {results.map(place => (
           <Text key={place.id}>
@@ -94,12 +107,12 @@ class Results extends Component {
           </Text>
         ))}
       </View>
-    )
+    );
   }
 }
 
 Results.propTypes = {
-  selectedPlaces: PropTypes.array.isRequired,
-}
+  selectedPlaces: PropTypes.array.isRequired
+};
 
-export default Results
+export default Results;
