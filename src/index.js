92861a667f506baaecb4126f5bcb2b4b41c43975
Fredrik Salin <fredrik.salin@netlight.com>
2018-04-06 18:05:41 +0200

    Fixed diff because of different prettier config

--- src/index.js
+++ src/index.js
@@ -1,66 +1,63 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-import VotingStep from "./votingStep";
-import Results from "./results";
-import places from "./places";
+import VotingStep from './votingStep'
+import Results from './results'
+import places from './places'
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      selectedPlaces: []
-    };
+      selectedPlaces: [],
+    }
   }
 
   getRandomPlaces = () => {
-    const currentPlaces = [];
-    let count = 0;
+    const currentPlaces = []
+    let count = 0
 
     while (count < 4) {
-      const randomPlace = places[Math.floor(Math.random() * places.length)];
+      const randomPlace = places[Math.floor(Math.random() * places.length)]
       const notAdded =
-        !this.state.selectedPlaces.includes(randomPlace.id) &&
-        !currentPlaces.includes(randomPlace);
+        !this.state.selectedPlaces.includes(randomPlace.id) && !currentPlaces.includes(randomPlace)
 
       if (notAdded) {
-        currentPlaces.push(randomPlace);
-        count++;
+        currentPlaces.push(randomPlace)
+        count++
       }
     }
-    return currentPlaces;
-  };
+    return currentPlaces
+  }
 
   voteForItem = itemId => {
     return fetch(`https://nameless-depths-10115.herokuapp.com/vote/${itemId}`, {
-      method: "POST"
+      method: 'POST',
     })
       .then(response => {
-        return response.json();
+        return response.json()
       })
       .catch(error => {
-        console.error(error);
-      });
-  };
+        console.error(error)
+      })
+  }
 
   setSelectedPlace = item => {
     this.setState({
-      selectedPlaces: [...this.state.selectedPlaces, item.id]
-    });
-    return this.voteForItem(item.id);
-  };
+      selectedPlaces: [...this.state.selectedPlaces, item.id],
+    })
+    return this.voteForItem(item.id)
+  }
 
   render() {
-    const { selectedPlaces } = this.state;
-    const places = this.getRandomPlaces();
+    const { selectedPlaces } = this.state
+    const places = this.getRandomPlaces()
 
     if (selectedPlaces.length === 4) {
-      return <Results selectedPlaces={selectedPlaces} />;
+      return <Results selectedPlaces={selectedPlaces} />
     }
-    return (
-      <VotingStep places={places} onItemSelected={this.setSelectedPlace} />
-    );
+    return <VotingStep places={places} onItemSelected={this.setSelectedPlace} />
   }
 }
 
-export default App;
+export default App
