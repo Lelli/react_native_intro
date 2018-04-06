35dcf4e6a4e408a20198937762d235f8f8971567
Fredrik Salin <fredrik.salin@netlight.com>
2018-04-06 19:38:42 +0200

    Added files for simpler animation example

---
+++ src/indexFadeInView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import FadeInView from './fadeInView'
+
+class App extends Component {
+  render() {
+    return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} />
+  }
+}
+
+export default App
